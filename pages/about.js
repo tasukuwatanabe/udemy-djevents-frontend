9f61345bbe659d92c1892b36f8bfb257ea00a2d0
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-03 12:50:09 +0900

    Layout, Header and Footer

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
+import Layout from '../components/Layout'
+
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title='About DJ Events'>
       <h1>About</h1>
-      <p>This is an app to find the lates DJ and other musical events.</p>
-    </div>
+    </Layout>
   )
 }
