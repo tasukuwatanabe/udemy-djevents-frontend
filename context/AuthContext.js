644b467f2a78251e669d8fc5b0ec2b86f52c5d5e
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-08 16:32:24 +0900

    Persist Logged In User

---
+++ context/AuthContext.js
@@ -0,0 +1,68 @@
+import { createContext, useState, useEffect } from "react";
+import { Router, useRouter } from "next/router";
+import { NEXT_URL } from "@/config/index";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  useEffect(() => checkUserLoggedIn(), []);
+
+  // Register user
+  const register = async (user) => {
+    console.log(user);
+  };
+
+  // Login user
+  const login = async ({ email: identifier, password }) => {
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+      router.push("/account/dashboard");
+    } else {
+      setError(data.message);
+      setError(null);
+    }
+  };
+
+  // Logout
+  const logout = async () => {
+    console.log("Logout");
+  };
+
+  // Check if user is logged in
+  const checkUserLoggedIn = async (user) => {
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
--- pages/account/register.js
+++ pages/account/register.js
@@ -1,58 +1,83 @@
-import {FaUser} from 'react-icons/fa'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { useState, useEffect, useContext } from 'react'
-import Link from 'next/link'
-import Layout from '@/components/Layout'
-import styles from '@/styles/AuthForm.module.css'
+import { FaUser } from "react-icons/fa";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState, useEffect, useContext } from "react";
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import AuthContext from "@/context/AuthContext";
+import styles from "@/styles/AuthForm.module.css";
 
 export default function RegisterPage() {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const { register, error } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (password !== passwordConfirm) {
-      toast.error('Passwords do no much!')
-      return
+      toast.error("Passwords do no much!");
+      return;
     }
 
-    console.log({ username, email, password });
-  }
+    register({ username, email, password });
+  };
 
   return (
-    <Layout title='User Registeration'>
+    <Layout title="User Registeration">
       <div className={styles.auth}>
         <h1>
-          <FaUser/> Register
+          <FaUser /> Register
         </h1>
         <ToastContainer />
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="email">Email Address</label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="passwordConfirm">Confirm Password</label>
-            <input type="password" id="passwordConfirm" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
+            <input
+              type="password"
+              id="passwordConfirm"
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+            />
           </div>
 
-          <input type='submit' value='Login' className='btn' />
+          <input type="submit" value="Login" className="btn" />
 
-          <p>Already have an account? <Link href='/account/login'>Login</Link></p>
+          <p>
+            Already have an account? <Link href="/account/login">Login</Link>
+          </p>
         </form>
       </div>
     </Layout>
-  )
+  );
 }
